e5d178b88e58cea9b433971e032eedc4c78891f1
Fuad Muhammed <62259225+G4me8woy@users.noreply.github.com>
2021-08-16 03:23:02 +0000

    update

--- sliders/no btn - auto/script/script.js
+++ sliders/no btn - auto/script/script.js
@@ -1,44 +1,29 @@
 var items = document.querySelectorAll('.item');
-
-
-
-// for (let i = 0; i < items.length; i++) {
-//     const element = items[i];
-
-//     if (i === 0) {
-//         element.style.zIndex = items.length;
-//     }
-//     else{
-//         element.style.zIndex = i - 1;
-//     }
-    
-// }
-
+var delayTime = 3000;
 var slideInIndex = 0;
 var slideOutIndex = items.length-1;
 
-
 setInterval(function slide() {
 
+    items[slideOutIndex].style.zIndex = 0;
+    items[slideInIndex].style.zIndex = 2;
     items[slideInIndex].classList.toggle('show');
-    items[slideOutIndex].classList.add('lower');
     if (slideInIndex == items.length - 1) {
         slideInIndex = 0;
     }else{
         slideInIndex++;
-    }
-    
-}, 3000)
+    }  
+}, delayTime)
 
 
 setTimeout(() => {
     setInterval(() => {
-        items[slideOutIndex].classList.remove('show');
-        items[slideOutIndex].classList.remove('lower');
+        items[slideOutIndex].classList.remove('show'); 
+        // items[slideOutIndex].classList.remove('lower');
         if (slideOutIndex == items.length-1) {
             slideOutIndex = 0;
         }else{
             slideOutIndex ++;
         }
-    }, 3000);
-}, 1000);
+    }, delayTime);
+}, 2000); // this is the delay time for hiding items, it should always be lower than delayTime
